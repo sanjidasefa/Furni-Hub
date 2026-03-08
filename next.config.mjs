0d668cea2526173a3baab83998fc37a53d17cbcd
sanjidasefa <sanjidasefa9@gmail.com>
2026-03-08 13:48:08 +0600

    add img url

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'images.pexels.com', 
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
         hostname: 'via.placeholder.com',
         pathname: '**',
       },
